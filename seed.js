9760c9200949235a7c59dc396111d0202a1d1f0a
unknown <diegovillagrasa@gmail.com>
2018-01-20 20:41:18 +0100

    seed

--- seed.js
+++ seed.js
@@ -3,11 +3,3 @@ var Seed = require('n2n').Seed;
 var seed = new Seed();
 seed.listen(6785);
 console.log('Seed listening 6785...');
-
-node.on('online', function () {
-    console.log('I am online:', this.id);
-  });
-
-node.on('node::online', function (newNode) {
-    console.log('Someone is online:', newNode.id);
-  });

5cc8ec6dd8532c2760ab07ba159c8bcf3030c11e
unknown <diegovillagrasa@gmail.com>
2018-01-21 13:04:21 +0100

    Using the net library for sockets

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-var networ = require("./network")
+var network = require("./network")
+
+initialize()
+
+
+async function initialize(){
+    await network.mapPort()
+    network.startServer()
+}
+
+async function exitHandler() {
+    await network.unMapPort()
+    network.stopServer()
+    process.exit()
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind());
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind());
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind());
--- network.js
+++ network.js
@@ -1,11 +1,77 @@
-var Node = require('n2n').Node;
-var node = new Node(6785);
-node.connect([{ host: '10.8.117.131', port: 6785 }]);
+const net = require('net');
+var natUpnp = require('nat-upnp');
 
-node.on('online', function () {
-  console.log('I am online:', this.id);
+const SPort = 8621
+
+//CReate a new UPNP client
+var client = natUpnp.createClient();
+
+//Open port using UPNP
+module.exports.mapPort = function(){
+  return new Promise(function(resolve, reject){
+    client.portMapping({
+      public: SPort,
+      private: SPort,
+      ttl: 1000
+    }, function(err) {
+      console.log(err)
+      console.log("Mapped")
+      resolve()
+    })
+  })
+}
+
+//Close port using UPNP
+module.exports.unMapPort = function(){
+  return new Promise(function(resolve, reject){
+    client.portUnmapping({
+      public: SPort
+    }, function(err) {
+      console.log(err)
+      console.log("unMapped")
+      resolve()
+    })
+  })
+}
+
+var server = net.createServer(function(socket) {
+  socket.write('Welcome\r\n');
+  console.log(socket.address())
+  
+	socket.on('connect', (e) => {
+    console.log('Socket has been created')
+    
+    //console.log(e)
+  })
+
+  socket.on('close', (e) => {
+    console.log('Socket has been closed')
+    console.log(e)
+  })
+
+  socket.on('data', (data) => {
+    console.log(data)
+  })
+
+  socket.on('end', (e) => {
+    console.log("Socket has been disconnected")
+    console.log(e)
+  })
 });
 
-node.on('node::online', function (newNode) {
-  console.log('Someone is online:', newNode.id);
-});
+module.exports.startServer = function(){
+  server.listen(SPort, '0.0.0.0')
+  console.log("Server started")
+  console.log(server.address())
+}
+
+module.exports.stopServer = function(){
+  server.close()
+  console.log("Server closed")
+}
+
+
+
+
+ 
+
